9a8619786795f40795d348c2e80bbd77690a1f5b
Harshvardhan <hv9796923@gmail.com>
2024-11-17 19:27:42 +0530

    second update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-<<<<<<< HEAD
-import Signup from './components/Signup';
-import Login from './components/Login';
-import Home from './components/Home';
-=======
-
->>>>>>> 9733fe4276fd0ea0b076f3fee8bf4896a305ac5e
 
 
 function App() {
