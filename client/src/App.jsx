34da08c07d820d8a2115e61769226b7f256cd1af
Harshvardhan <hv9796923@gmail.com>
2024-11-17 19:27:02 +0530

    update after pull

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,24 +11,7 @@ import Home from './components/Home';
 
 function App() {
 
-<<<<<<< HEAD
-  return (
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
-      <div className='w-full block'><Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
-    </div>
-    </div>
-   
-  )
-=======
   return (<></> )
->>>>>>> 9733fe4276fd0ea0b076f3fee8bf4896a305ac5e
 }
 
 export default App
