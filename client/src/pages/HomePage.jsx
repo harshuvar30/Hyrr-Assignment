42cbe70fe995d480423aeb1493f71263c34c23a5
Harshvardhan_3wour <harshvardhan.harshvardhan@firstsource.com>
2024-12-06 19:44:37 +0530

    Started working on homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className='mt-4 flex flex-col gap-4'>
+      {/* BREADCRUMB */}
+      <div className='flex gap-4'>
+        <Link to='/'>Home</Link>
+        <span>.</span>
+        <span className='text-blue-800'>Blogs and Articles</span>
+      </div>
+      {/* INTRODUCTION */}
+      <div className='flex items-center justify-between'>
+        {/* titles */}
+        <div className=''></div>
+      </div>
+      {/* FEATURED POSTS */}
+      {/* POST LIST */}
+    </div>
   )
 }
 
